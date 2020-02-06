95e97c2ac63413387d35e4349c2c7ffa3221ffce
azlemaster <azzeddinelem@gmail.com>
2020-02-06 10:43:53 +0100

    init server template

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,26 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "require": true,
-        "process": true,
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+    "extends": "standard"
 };
--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-//const {sequelize} = require('./models')
+const {sequelize} = require('./models')
 const config = require('./config/config')
 
 const app = express()
@@ -10,16 +10,12 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-//require('./passport')
+require('./passport')
 
-//require('./routes')(app)
+require('./routes')(app)
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
-});
-
-app.listen(config.port, () => {
-    console.log(`Running on http://localhost:${config.port}`)
-})
-
+sequelize.sync({force: false})
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
