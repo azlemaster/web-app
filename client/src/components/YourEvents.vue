5233fe7c5e742551376511d211ba8826b6a5eeb0
qld49 <quentin.taffu@gmail.com>
2020-02-17 19:50:24 +0100

    dev view your events

--- client/src/components/YourEvents.vue
+++ client/src/components/YourEvents.vue
@@ -1,23 +1,86 @@
 <template>
-  <panel :title="user.name">
-    <v-layout>
-      <v-flex xs12>
-      </v-flex> 
-    </v-layout>
+  <panel title="Bookmarks">
+    <v-data-table
+      :headers="headers"
+      :pagination.sync="pagination"
+      :items="bookmarks">
+      <template slot="items" scope="props">
+        <td class="text-xs-right">
+          {{props.item.title}}
+        </td>
+        <td class="text-xs-right">
+          {{props.item.owner}}
+        </td>
+        <td class="text-xs-right">
+          <v-btn
+            dark
+            class="cyan"
+            :to="{
+              name: 'event', 
+              params: {
+                eventId: props.item.id
+              }
+            }">
+            View
+          </v-btn>
+        </td>
+        <td  style="width:90%; padding:0%;">
+          <img class="event-image" :src="props.item.eventImageUrl"/>
+        </td>
+      </template>
+    </v-data-table>
   </panel>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
+
 export default {
-  components: {
-  },
   data () {
     return {
+      headers: [
+        {
+          text: 'Title',
+          value: 'title'
+        },
+        {
+          text: 'Owner',
+          value: 'owner'
+        },
+        {
+          text: 'Visit',
+          value: 'visit'
+        },
+        {
+          text: '',
+          value: 'eventImageUrl'
+        }
+      ],
+      pagination: {
+        sortBy: 'createdAt',
+        descending: true
+      },
+      bookmarks: []
+    }
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn',
+      'user'
+    ])
+  },
+  async mounted () {
+    if (this.isUserLoggedIn) {
+      this.bookmarks = (await BookmarksService.index()).data
     }
   }
 }
 </script>
 
 <style scoped>
-
-</style>
+.event-image {
+  width: 40%;
+  margin: 0 auto;
+}
+</style>>
